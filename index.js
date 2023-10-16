f8065386e4fb8d8f37d03be812ba163c0831cad4
FajnyKuba784 <126417803+FajnyKuba784@users.noreply.github.com>
2023-10-16 16:28:06 +0200

    z

---
+++ index.js
@@ -0,0 +1,60 @@
+const endpointskelp = "http://localhost/wordpresstest"
+
+const endpointhurtownia = "http://192.168.15.7/kgwordpress/wp-json/wc/v3/orders"
+
+const idproduktusklep = 42 
+const idproduktuhurtownia = 35
+var dane_zam = {}
+
+async function getprodukt(){
+
+    const data = await fetch(endpointskelp+"/wp-json/wc/v3/products/42",{
+    headers: {
+        Authorization: `Basic ${btoa("FajnyKuba784:Paruwkatoja150")}`
+    }
+})
+    const json = await data.json()
+    const iloscproduktowsklep = json.stock_quantity
+    if(iloscproduktowsklep<=10&& dane_zam.id==undefined){
+        console.log("potrzebne zamowienie")
+        zamowienie()
+    }
+    console.log(iloscproduktowsklep)
+
+
+
+
+}
+
+async function zamowienie(){
+    const url = new URL(endpointhurtownia)
+
+    const body = {
+        "lineitems": {"product_id": 34, "qunatity": 134}
+    }
+
+   /* for(let i in params){
+
+        url.searchParams.append(i,params[i])
+
+
+    }*/
+    const data = await fetch(url,{
+        method: "POST",
+        body: body,
+        headers: {
+            Authorization: `Basic ${btoa("hurtownia:hurtownia")}`,
+            "Content-Type": "application/json"
+        }
+    })
+    dane_zam = data.json()
+
+}
+
+getprodukt()
+setInterval(()=>{
+
+    getprodukt()
+
+
+},5000)

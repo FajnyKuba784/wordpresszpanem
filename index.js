ff35edffe60e3308449622c5003c543e106e8338
FajnyKuba784 <126417803+FajnyKuba784@users.noreply.github.com>
2023-10-19 09:54:46 +0200

    DZIALA:00000

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const endpointhurtownia = "http://192.168.15.7/kgwordpress/wp-json/wc/v3/orders"
 
 const idproduktusklep = 42 
 const idproduktuhurtownia = 35
+var iloscproduktowsklep = 0
 var dane_zam = {}
 
 async function getprodukt(){
@@ -14,10 +15,15 @@ async function getprodukt(){
     }
 })
     const json = await data.json()
-    const iloscproduktowsklep = json.stock_quantity
+    iloscproduktowsklep = json.stock_quantity
     if(iloscproduktowsklep<=10 && dane_zam.id==undefined){
         console.log("potrzebne zamowienie")
         zamowienie()
+    }
+    
+    if(dane_zam!= undefined){
+        checkOrder()
+    
     }
     console.log(iloscproduktowsklep)
 
@@ -31,7 +37,7 @@ async function zamowienie(){
 
     var body = {
         "line_items": [
-        {"product_id": 35, "quantity": 69}
+        {"product_id": 35, "quantity": 666}
     ]}
 
    /* for(let i in params){
@@ -49,9 +55,38 @@ async function zamowienie(){
         }
     })
     dane_zam = await data.json()
+   
 
 }
+async function checkOrder(){
+     //console.log( dane_zam.id)
+    const url = new URL(endpointhurtownia+"/"+dane_zam.id)
+    const data = await fetch(url,{
+        headers: {
+            Authorization: `Basic ${btoa("hurtownia:hurtownia")}`
+        }
+    })
 
+   dane_zam = await data.json()
+  console.log(dane_zam.status)
+  if(dane_zam.status=="completed"){
+
+    const body = {"stock_quantity":iloscproduktowsklep+69}
+
+    const data = await fetch(endpointskelp+"/wp-json/wc/v3/products/42",{
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            Authorization: `Basic ${btoa("FajnyKuba784:Paruwkatoja150")}`,
+            "Content-Type": "application/json"
+        } 
+
+
+    })
+    const json = await data.json()
+    dane_zam = {}
+    }
+}
 setInterval(()=>{
     
     getprodukt()
